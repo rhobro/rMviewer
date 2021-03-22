4d312712e3cc0713ff3684b52bba399be7cd6cf2
Rohan Mathew <robozmat@gmail.com>
2021-03-22 17:19:33 +0000

    finished

--- rmviewer.go
+++ rmviewer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/rhobro/goutils/pkg/fileio"
 	"io"
 	"os"
@@ -15,17 +16,39 @@ func init() {
 func main() {
 	defer os.RemoveAll(fileio.TmpDir)
 
-	// check python
-	if !strings.Contains(Exec("python3", "--version"), "Python") {
+	// check rmview
+	if strings.Contains(Exec("where", "rmview"), "not found") {
+		// check python
+		fmt.Println("Checking for a Python 3 installation")
+		if !strings.Contains(Exec("python3", "--version"), "Python") {
 
-		// check brew
-		if !strings.Contains(Exec("brew", "--version"), "Homebrew") {
-			Exec("/bin/bash", "-c", "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
+			// check brew
+			fmt.Println("Checking for a Homebrew installation")
+			if !strings.Contains(Exec("brew", "--version"), "Homebrew") {
+				fmt.Println("Installing Homebrew")
+				Exec("/bin/bash", "-c", "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
+			}
+
+			// install python
+			fmt.Println("Installing Python 3")
+			Exec("brew", "install", "python")
 		}
 
-		// install python
-		Exec("brew", "install", "python")
+		fmt.Println("Checking for a Git installation")
+		if !strings.Contains(Exec("git", "--version"), "version") {
+			fmt.Println("Installing git")
+			Exec("brew", "install", "git")
+		}
+
+		fmt.Println("Downloading rMview")
+		Exec("git", "clone", "https://github.com/bordaigorl/rmview.git", fileio.TmpDir)
+		fmt.Println("Installing rMview")
+		Exec("pip3", "install", fileio.TmpDir)
 	}
+
+	// launch rmview
+	fmt.Println("Launching rMview")
+	Exec("rmview")
 }
 
 func Exec(cmd string, args ...string) string {
